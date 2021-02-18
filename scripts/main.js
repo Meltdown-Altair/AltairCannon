b02ebe4d61b9440cda9413742ae764ed58391679
Roomba <61640498+Meltdown-Altair@users.noreply.github.com>
2021-02-18 20:24:52 +0700

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,39 @@
+//units
+require("units/testunit4");
+
+//blocks
+require("blocks/activationmatter-weaver");
+require("blocks/atmospheric-cooler");
+require("blocks/impact-projector");
+require("blocks/multi-projector");
+require("blocks/multi-cultivator");
+require("blocks/power-processor");
+require("blocks/coredvalut");
+require("blocks/boostwater");
+require("blocks/plastsalvo");
+require("blocks/titanium-launch-pad")
+
+//experiement
+require("experiment/page");
+require("experiment/oregen");
+
+//campaign
+require("campaign/planet");
+
+//units
+require("units/testunit4");
+
+print("Load Complete!");
+
+
+
+
+
+
+/*
+credit(){
+  younggam: "answering tons of question",
+  deltaNedas: "refer to routorio, rtfm",
+  sk7725: "answering tons of question, refer to testers"
+}
+*/
